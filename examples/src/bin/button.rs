fe01b6e98686c9904641a5775a77d67e0494c0e3
Felipe Balbi <febalbi@microsoft.com>
2025-11-21 08:12:58 -0800

    examples: button: remove left-over OSTIMER initialization
    While at that, also cargo fmt

--- examples/src/bin/button.rs
+++ examples/src/bin/button.rs
@@ -4,27 +4,14 @@
 use embassy_executor::Spawner;
 use embassy_mcxa::bind_interrupts;
 use embassy_time::Timer;
+use hal::gpio::{DriveStrength, Input, Pull, SlewRate};
 use {defmt_rtt as _, embassy_mcxa as hal, panic_probe as _};
-
-use hal::gpio::{Input, Pull, DriveStrength, SlewRate};
-
-// Bind only OS_EVENT for timer interrupts
-bind_interrupts!(struct Irqs {
-    OS_EVENT => hal::ostimer::time_driver::OsEventHandler;
-});
-
-#[used]
-#[no_mangle]
-static KEEP_OS_EVENT: unsafe extern "C" fn() = OS_EVENT;
 
 #[embassy_executor::main]
 async fn main(_spawner: Spawner) {
     let p = hal::init(hal::config::Config::default());
 
     defmt::info!("Button example");
-
-   // Initialize embassy-time global driver backed by OSTIMER0
-    hal::ostimer::time_driver::init(hal::config::Config::default().time_interrupt_priority, 1_000_000);
 
     let mut monitor = Input::new(p.P1_7, Pull::Disabled, DriveStrength::Normal, SlewRate::Slow);
 
